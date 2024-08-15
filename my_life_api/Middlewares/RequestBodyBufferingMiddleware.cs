a1ec419a7ff1055cfd3242ec7ffdf71a774cf683
Fernando Fillmann <fernando@refere.com.br>
2024-08-15 15:06:39 -0300

    FEAT - Inclusao de middleware para permitir reuso do body da request multiplas vezes

---
+++ my_life_api/Middlewares/RequestBodyBufferingMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Immutable;
+
+public class RequestBodyBufferingMiddleware
+{
+    private readonly RequestDelegate _next;
+    public static readonly ImmutableList<string> methodsWithBody = ImmutableList.Create("POST", "PUT");
+
+    public RequestBodyBufferingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (methodsWithBody.Contains(context.Request.Method))
+        {
+            // Habilita o bufferamento para ler o corpo da requisição múltiplas vezes
+            context.Request.EnableBuffering();
+        }
+
+        await _next(context);
+    }
+}
