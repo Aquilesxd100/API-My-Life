8e769df27351251b2634bc3d8ce4884d5c3c6f31
Fernando Fillmann <fernando@refere.com.br>
2024-09-17 13:08:40 -0300

    REFACTOR - Padronizacao de nomenclatura do banco

--- my_life_api/SQL/TableCreations.sql
+++ my_life_api/SQL/TableCreations.sql
@@ -4,17 +4,17 @@ Use patholo1_my_life;
 
 Create Table Registries (
   id Integer Auto_Increment Primary Key,
-  created_at Timestamp,
+  createdAt Timestamp,
   year Year Not Null,
   content Text Not Null,
-  main_image_url Varchar(255)
+  mainImageUrl Varchar(255)
 );
 
 Create Table SecondaryImages (
   id Integer Auto_Increment Primary Key,
   imageUrl Varchar(255) Not Null,
-  registry_id Integer Not Null,
-  Foreign Key (registry_id) References Registries(id)
+  registryId Integer Not Null,
+  Foreign Key (registryId) References Registries(id)
 );
 
 -- Registros de conteudos vistos e seus recursos
@@ -28,16 +28,16 @@ Create Table Authors (
   id Integer Auto_Increment Primary Key,
   name Varchar(50) Not Null,
   imageUrl Varchar(255),
-  content_type_id Integer Not Null,
-  Foreign Key (content_type_id) References ContentTypes(id)
+  contentTypeId Integer Not Null,
+  Foreign Key (contentTypeId) References ContentTypes(id)
 );
 
 Create Table Categories (
   id Integer Auto_Increment Primary Key,
   name Varchar(50) Not Null,
   iconBase64 Text,
-  content_type_id Integer Not Null,
-  Foreign Key (content_type_id) References ContentTypes(id)
+  contentTypeId Integer Not Null,
+  Foreign Key (contentTypeId) References ContentTypes(id)
 );
 
 Create Table Movies (
@@ -46,52 +46,52 @@ Create Table Movies (
   imageUrl Varchar(255),
   rating Decimal(3,1) Not Null,
   dubbed Tinyint(1) Default 0, 
-  soul_fragment Tinyint(1) Default 0,
-  author_id Integer Not Null,
-  Foreign Key (author_id) References Authors(id)
+  soulFragment Tinyint(1) Default 0,
+  authorId Integer Not Null,
+  Foreign Key (authorId) References Authors(id)
 );
 
 Create Table Movie_x_Category (
   id Integer Auto_Increment Primary Key,
-  movie_id Integer Not Null,
-  Foreign Key (movie_id) References Movies(id),
-  category_id Integer Not Null,
-  Foreign Key (category_id) References Categories(id)
+  movieId Integer Not Null,
+  Foreign Key (movieId) References Movies(id),
+  categoryId Integer Not Null,
+  Foreign Key (categoryId) References Categories(id)
 );
 
 Create Table Musics (
   id Integer Auto_Increment Primary Key,
   name Varchar(70) Not Null,
   rating Decimal(3,1) Not Null,
-  soul_fragment Tinyint(1) Default 0,
+  soulFragment Tinyint(1) Default 0,
   link Varchar(255),
-  author_id Integer Not Null,
-  Foreign Key (author_id) References Authors(id)
+  authorId Integer Not Null,
+  Foreign Key (authorId) References Authors(id)
 );
 
 Create Table Music_x_Category (
   id Integer Auto_Increment Primary Key,
-  music_id Integer Not Null,
-  Foreign Key (music_id) References Musics(id),
-  category_id Integer Not Null,
-  Foreign Key (category_id) References Categories(id)
+  musicId Integer Not Null,
+  Foreign Key (musicId) References Musics(id),
+  categoryId Integer Not Null,
+  Foreign Key (categoryId) References Categories(id)
 );
 
 Create Table Phrases (
   id Integer Auto_Increment Primary Key,
   phrase Tinytext Not Null,
   rating Decimal(3,1) Not Null, 
-  soul_fragment Tinyint(1) Default 0,
-  author_id Integer Not Null,
-  Foreign Key (author_id) References Authors(id)
+  soulFragment Tinyint(1) Default 0,
+  authorId Integer Not Null,
+  Foreign Key (authorId) References Authors(id)
 );
 
 Create Table Phrase_x_Category (
   id Integer Auto_Increment Primary Key,
-  phrase_id Integer Not Null,
-  Foreign Key (phrase_id) References Phrases(id),
-  category_id Integer Not Null,
-  Foreign Key (category_id) References Categories(id)
+  phraseId Integer Not Null,
+  Foreign Key (phraseId) References Phrases(id),
+  categoryId Integer Not Null,
+  Foreign Key (categoryId) References Categories(id)
 );
 
 Create Table Series (
@@ -100,17 +100,17 @@ Create Table Series (
   imageUrl Varchar(255),
   rating Decimal(3,1), 
   completed Tinyint(1) Default 1,
-  soul_fragment Tinyint(1) Default 0,
-  author_id Integer Not Null,
-  Foreign Key (author_id) References Authors(id)
+  soulFragment Tinyint(1) Default 0,
+  authorId Integer Not Null,
+  Foreign Key (authorId) References Authors(id)
 );
 
 Create Table Serie_x_Category (
   id Integer Auto_Increment Primary Key,
-  serie_id Integer Not Null,
-  Foreign Key (serie_id) References Series(id),
-  category_id Integer Not Null,
-  Foreign Key (category_id) References Categories(id)
+  serieId Integer Not Null,
+  Foreign Key (serieId) References Series(id),
+  categoryId Integer Not Null,
+  Foreign Key (categoryId) References Categories(id)
 );
 
 Create Table Mangas (
@@ -119,17 +119,17 @@ Create Table Mangas (
   imageUrl Varchar(255),
   rating Decimal(3,1), 
   completed Tinyint(1) Default 1,
-  soul_fragment Tinyint(1) Default 0,
-  author_id Integer Not Null,
-  Foreign Key (author_id) References Authors(id)
+  soulFragment Tinyint(1) Default 0,
+  authorId Integer Not Null,
+  Foreign Key (authorId) References Authors(id)
 );
 
 Create Table Manga_x_Category (
   id Integer Auto_Increment Primary Key,
-  manga_id Integer Not Null,
-  Foreign Key (manga_id) References Mangas(id),
-  category_id Integer Not Null,
-  Foreign Key (category_id) References Categories(id)
+  mangaId Integer Not Null,
+  Foreign Key (mangaId) References Mangas(id),
+  categoryId Integer Not Null,
+  Foreign Key (categoryId) References Categories(id)
 );
 
 Create Table Animes (
@@ -138,17 +138,17 @@ Create Table Animes (
   imageUrl Varchar(255),
   rating Decimal(3,1), 
   completed Tinyint(1) Default 1,
-  soul_fragment Tinyint(1) Default 0,
-  author_id Integer Not Null,
-  Foreign Key (author_id) References Authors(id)
+  soulFragment Tinyint(1) Default 0,
+  authorId Integer Not Null,
+  Foreign Key (authorId) References Authors(id)
 );
 
 Create Table Anime_x_Category (
   id Integer Auto_Increment Primary Key,
-  anime_id Integer Not Null,
-  Foreign Key (anime_id) References Animes(id),
-  category_id Integer Not Null,
-  Foreign Key (category_id) References Categories(id)
+  animeId Integer Not Null,
+  Foreign Key (animeId) References Animes(id),
+  categoryId Integer Not Null,
+  Foreign Key (categoryId) References Categories(id)
 );
 
 Create Table Games (
@@ -157,17 +157,17 @@ Create Table Games (
   imageUrl Varchar(255),
   rating Decimal(3,1), 
   completed Tinyint(1) Default 1,
-  soul_fragment Tinyint(1) Default 0,
-  author_id Integer Not Null,
-  Foreign Key (author_id) References Authors(id)
+  soulFragment Tinyint(1) Default 0,
+  authorId Integer Not Null,
+  Foreign Key (authorId) References Authors(id)
 );
 
 Create Table Game_x_Category (
   id Integer Auto_Increment Primary Key,
-  game_id Integer Not Null,
-  Foreign Key (game_id) References Games(id),
-  category_id Integer Not Null,
-  Foreign Key (category_id) References Categories(id)
+  gameId Integer Not Null,
+  Foreign Key (gameId) References Games(id),
+  categoryId Integer Not Null,
+  Foreign Key (categoryId) References Categories(id)
 );
 
 Create Table Books (
@@ -176,17 +176,17 @@ Create Table Books (
   imageUrl Varchar(255),
   rating Decimal(3,1), 
   completed Tinyint(1) Default 1,
-  soul_fragment Tinyint(1) Default 0,
-  author_id Integer Not Null,
-  Foreign Key (author_id) References Authors(id)
+  soulFragment Tinyint(1) Default 0,
+  authorId Integer Not Null,
+  Foreign Key (authorId) References Authors(id)
 );
 
 Create Table Book_x_Category (
   id Integer Auto_Increment Primary Key,
-  book_id Integer Not Null,
-  Foreign Key (book_id) References Books(id),
-  category_id Integer Not Null,
-  Foreign Key (category_id) References Categories(id)
+  bookId Integer Not Null,
+  Foreign Key (bookId) References Books(id),
+  categoryId Integer Not Null,
+  Foreign Key (categoryId) References Categories(id)
 );
 
 
