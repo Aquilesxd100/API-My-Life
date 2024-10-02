29487e1af539a00aa4593a42b2b698bbcdfee9bb
Fernando Fillmann <fernando@refere.com.br>
2024-10-02 14:09:22 -0300

    FEAT - Criação de classe para armazenamento de filtros e filtragem em si de dados na query SQL

---
+++ my_life_api/Models/ResourceFilters.cs
@@ -0,0 +1,71 @@
+﻿namespace my_life_api.Models
+{
+    public class ResourceFilters
+    {
+        // Filtros globais
+        public bool? soulFragment { get; set; }
+
+        public int? authorId { get; set; }
+
+        public IEnumerable<int>? categoriesIds { get; set; }
+
+        public float? ratingGreaterEqualTo { get; set; }
+
+        public float? ratingLesserEqualTo { get; set; }
+
+        // Filtros globais, com exceção de Frases
+        public string? name { get; set; }
+
+        // Filtros de séries, animes, mangás, jogos e livros
+        public bool? completed { get; set; }
+
+        // Filtros de filmes e séries
+        public bool? dubbed { get; set; }
+
+        public string MountConditionalQueryPart(string? categoryRelationTableAlias) {
+            string conditionalQueryPart = "";
+            List<string> conditionals = new List<string>();
+
+            if (authorId != null) 
+                conditionals.Add($"authorId = {authorId}");
+
+            if (
+                categoriesIds != null
+                && !string.IsNullOrEmpty(categoryRelationTableAlias)
+            ) {
+                string formatedCategoriesIds = String.Join(", ", categoriesIds);
+                conditionals.Add($"{categoryRelationTableAlias}.categoryId In ({formatedCategoriesIds})");
+            }
+
+            if (soulFragment != null) {
+                int soulFragmentInByte = soulFragment == true ? 1 : 0;
+                conditionals.Add($"soulFragment = {soulFragmentInByte}");
+            }
+
+            if (dubbed != null) {
+                int dubbedInByte = dubbed == true ? 1 : 0;
+                conditionals.Add($"dubbed = {dubbedInByte}");
+            }
+
+            if (completed != null) {
+                int completedInByte = completed == true ? 1 : 0;
+                conditionals.Add($"completed = {completedInByte}");
+            }
+
+            if (ratingGreaterEqualTo != null) 
+                conditionals.Add($"rating >= {ratingGreaterEqualTo}");
+
+            if (ratingLesserEqualTo != null)
+                conditionals.Add($"rating <= {ratingLesserEqualTo}");
+
+            if (!string.IsNullOrEmpty(name))
+                conditionals.Add($"name Like '{name}'");
+
+            if (conditionals.Count > 0) {
+                conditionalQueryPart = "Where " + String.Join(" And ", conditionals);
+            }
+
+            return conditionalQueryPart;
+        }
+    }
+}
